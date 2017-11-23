321c331542e7c413930ade0048c9a276bf18f275
select2webfacebookbot <33750264+select2webfacebookbot@users.noreply.github.com>
2017-11-23 17:14:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,16 +14,14 @@ if (isset($_GET['hub_verify_token'])) {
     }
 }
 
-/* receive and send messages */
-$input = json_decode(file_get_contents('php://input'), true);
-
-
 /* Debug data */
 $file = fopen("logs.txt","w"); 
-fwrite($file, $input['entry'][0]['messaging'][0]['sender']['id']) ); 
+fwrite( $file, file_get_contents('php://input') ); 
 fclose($file); 
 
 
+/* receive and send messages */
+$input = json_decode(file_get_contents('php://input'), true);
 
 if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
     
