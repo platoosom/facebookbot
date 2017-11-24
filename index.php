2972f2c1e62c6986d7b27db9b1e9be16a452e60b
select2webfacebookbot <33750264+select2webfacebookbot@users.noreply.github.com>
2017-11-24 12:27:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -19,17 +19,11 @@ if (isset($_GET['hub_verify_token'])) {
 /* receive and send messages */
 $input = json_decode(file_get_contents('php://input'), true);
 
-/* Debug data */
-$file = fopen("logs.txt","w");
-fwrite($file, file_get_contents('php://input')); 
-fclose($file);
-
-
 
 if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
     
     $sender = $input['entry'][0]['messaging'][0]['sender']['id']; //sender facebook id
-    $sticker_id = $input['entry'][0]['messaging'][0]['message']['sticker_id'];
+    $emoticon = $input['entry'][0]['messaging'][0]['message']['text'];
     
     $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='. $access_token;
 
@@ -42,7 +36,7 @@ if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
         "id":"' . $sender . '"
         },
         "message":{
-            "text": "Your sticker ID is '. $sticker_id .'"
+            "text": "Your emoticon is '. $emoticon .'"
         }
     }';
 
